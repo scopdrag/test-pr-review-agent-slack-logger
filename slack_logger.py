2b9c1f6b9a9c45aa12fb386c5b9b74f98745f318
Nikhil Yadav <codemonster34@gmail.com>
2025-06-12 13:54:43 +0530

    Update slack_logger.py

--- slack_logger.py
+++ slack_logger.py
@@ -67,6 +67,7 @@ class SlackFormatter(logging.Formatter):
         ret['title'] = record.name
         ret['ts'] = record.created
         ret['text'] = super(SlackFormatter, self).format(record)
+        print(ret['text'])
         return ret
 
 
